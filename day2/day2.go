dadc5cba6c009dfce2239befb15cd6e5d4260fae
Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2024-12-02 13:43:43 -0500

    Day 2 Part 1

--- day2/day2.go
+++ day2/day2.go
@@ -1,12 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 )
 
+func isReportSafe(s string) bool {
+	numbersAsStrings := strings.Fields(s)
+	var numbers []int
+	for i := 0; i < len(numbersAsStrings); i++ {
+		val, err := strconv.Atoi(numbersAsStrings[i])
+		if err != nil {
+			fmt.Println("Error converting")
+			os.Exit(1)
+		}
+		numbers = append(numbers, val)
+	}
+
+	isIncreasing := false
+	if numbers[0] < numbers[1] {
+		isIncreasing = true
+	}
+
+	for i := 0; i < len(numbers)-1; i++ {
+		if numbers[i] > numbers[i+1] && isIncreasing {
+			return false
+		}
+		if numbers[i] < numbers[i+1] && !isIncreasing {
+			return false
+		}
+		if math.Abs(float64(numbers[i])-float64(numbers[i+1])) > 3 || math.Abs(float64(numbers[i])-float64(numbers[i+1])) == 0 {
+			return false
+		}
+	}
+
+	fmt.Printf("Valid %s \n", s)
+	return true
+}
+
 func partOneSolution(file *os.File) int {
-	return 1
+	scanner := bufio.NewScanner(file)
+	sum := 0
+	for scanner.Scan() {
+		if isReportSafe(scanner.Text()) == true {
+			sum += 1
+		}
+	}
+	return sum
 }
 
 func partTwoSolution(file *os.File) int {
