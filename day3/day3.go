5d2921efc101bd84099ade99e780315cc693249b
Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2024-12-11 17:40:42 -0500

    day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -35,13 +35,10 @@ func parseValue(s string, idx int) int {
 		}
 	}
 
-	fmt.Println(n_two_s)
-	fmt.Println(n_one_s)
-
 	n_one, err := strconv.Atoi(n_one_s)
 	n_two, err_t := strconv.Atoi(n_two_s)
 	if err != nil || err_t != nil {
-		fmt.Println("Error converting.")
+		//fmt.Println("Error converting.")
 		return 0
 	}
 
@@ -62,6 +59,35 @@ func getLineValue(s string) int {
 	return total
 }
 
+var enabled = true
+
+func getLineValueIfEnabled(s string) int {
+	total := 0
+
+	for i := 0; i < len(s)-3; i++ {
+		if s[i:i+4] == "do()" {
+			enabled = true
+			//fmt.Println("Seen do()")
+			//fmt.Println(i)
+		}
+		if i+7 <= len(s) && s[i:i+7] == "don't()" {
+			enabled = false
+			//fmt.Println("Seen don't()")
+			//fmt.Println(i)
+		}
+		if s[i:i+4] == "mul(" {
+			if enabled {
+				total += parseValue(s, i)
+			}
+			if !enabled {
+				fmt.Printf("Not enabled but seen at character %d \n", i)
+			}
+			//total += parseValue(s, i)
+		}
+	}
+	return total
+}
+
 func partOneSolution(file *os.File) int {
 	sum := 0
 	scanner := bufio.NewScanner(file)
@@ -71,6 +97,18 @@ func partOneSolution(file *os.File) int {
 	}
 
 	return sum
+}
+
+func partTwoSolution(file *os.File) int {
+	scanner := bufio.NewScanner(file)
+
+	fullString := ""
+
+	for scanner.Scan() {
+		fullString += scanner.Text()
+	}
+
+	return getLineValueIfEnabled(fullString)
 }
 
 func main() {
@@ -83,5 +121,6 @@ func main() {
 
 	defer file.Close()
 
+	fmt.Println(partTwoSolution(file))
 	fmt.Println(partOneSolution(file))
 }
